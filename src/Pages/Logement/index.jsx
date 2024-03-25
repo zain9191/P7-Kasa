3b21b99f8c2f9209b60fe6921e718a97af19ff9c
Zain <zain.frayha@gmail.com>
2024-03-25 17:35:14 +0100

    working on logement page

--- src/Pages/Logement/index.jsx
+++ src/Pages/Logement/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../../utils/hooks";
+import "../../utils/style/sass/logement.scss";
 
 function Logement() {
   const { logementId } = useParams();
@@ -15,9 +16,21 @@ function Logement() {
           {data.map((logement) => {
             if (logement.id === logementId) {
               return (
-                <div key={logement.id}>
+                <div className="logement-main-div" key={logement.id}>
+                  <img
+                    className="logement-img"
+                    src={logement.cover}
+                    alt="logement cover "
+                  />
                   <h2>{logement.title}</h2>
-                  <p>{logement.description}</p>
+                  <h3>{logement.location}</h3>
+                  <div>
+                    {logement.tags.map((tag, index) => (
+                      <div key={index}>
+                        <span>{tag}</span>
+                      </div>
+                    ))}
+                  </div>
                 </div>
               );
             }
