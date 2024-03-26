970bfc8fe512e44f3cca93d340cb1d5e247f0143
Zain <zain.frayha@gmail.com>
2024-03-26 19:36:33 +0100

    added Rating component.

--- src/Pages/Logement/index.jsx
+++ src/Pages/Logement/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../../utils/hooks";
 import "../../utils/style/sass/logement.scss";
+import Rating from "../../components/Rating";
 
 function Logement() {
   const { logementId } = useParams();
@@ -22,14 +23,22 @@ function Logement() {
                     src={logement.cover}
                     alt="logement cover "
                   />
-                  <h2>{logement.title}</h2>
-                  <h3>{logement.location}</h3>
-                  <div>
+                  <h2 className="logement-title">{logement.title}</h2>
+                  <h3 className="logement-location">{logement.location}</h3>
+                  <div className="logement-tags-div">
                     {logement.tags.map((tag, index) => (
                       <div key={index}>
-                        <span>{tag}</span>
+                        <span className="logement-tags-span">{tag}</span>
                       </div>
                     ))}
+                  </div>
+
+                  <div className="logement-owner-rating">
+                    <div className="logement-rating-div">
+                      <Rating rating={parseInt(logement.rating)} />
+                    </div>
+
+                    <div className="logement-owner"></div>
                   </div>
                 </div>
               );
---
+++ src/components/Rating/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import emptyStar from "../../assets/emptyStar.png";
+import filledStar from "../../assets/filled-star.png";
+
+const Rating = ({ rating }) => {
+  const maxRating = 5;
+  let stars = [];
+
+  for (let i = 1; i <= maxRating; i++) {
+    stars.push(i <= rating ? filledStar : emptyStar);
+  }
+  console.log(emptyStar, filledStar);
+
+  return (
+    <div className="rating">
+      {stars.map((star, index) => (
+        <img key={index} src={star} alt="star" />
+      ))}
+    </div>
+  );
+};
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;
