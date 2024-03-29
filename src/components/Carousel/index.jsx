e694d895dc10eb78655241a87479dde76feadd46
Zain <zain.frayha@gmail.com>
2024-03-29 17:23:07 +0100

    fixed the Carousel, working as it should.

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,8 +1,17 @@
-import React, { useState } from "react";
-// import "./Carousel.css";
+import React, { useState, useEffect } from "react";
+import arrow_left from "../../assets/arrow-left.png";
+import arrow_right from "../../assets/arrow-right.png";
 
 const Carousel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [animation, setAnimation] = useState("");
+
+  useEffect(() => {
+    setAnimation("hide");
+    setTimeout(() => {
+      setAnimation("");
+    }, 10);
+  }, [currentIndex]); // Trigger animation whenever currentIndex changes
 
   const nextSlide = () => {
     setCurrentIndex((prevIndex) =>
@@ -19,24 +28,32 @@ const Carousel = ({ images }) => {
   return (
     <div className="carousel">
       <button
-        className="carousel__button carousel__button--prev"
+        className={`carousel__button carousel__button--prev ${animation}`}
         onClick={prevSlide}
       >
-        Prev
+        <img src={arrow_left} alt="Previous Slide" />
       </button>
-      <img
-        className="carousel__image"
-        src={images[currentIndex]}
-        alt={`Slide ${currentIndex}`}
-      />
+      <div className={`carousel__image-container ${animation}`}>
+        <img src={images[currentIndex]} alt={`Slide ${currentIndex}`} />
+      </div>
+      <div className="carousel__radios">
+        {images.map((image, index) => (
+          <input
+            key={index}
+            type="radio"
+            name="carousel-radio"
+            checked={index === currentIndex}
+            onChange={() => setCurrentIndex(index)}
+          />
+        ))}
+      </div>
       <button
-        className="carousel__button carousel__button--next"
+        className={`carousel__button carousel__button--next ${animation}`}
         onClick={nextSlide}
       >
-        Next
+        <img src={arrow_right} alt="Next Slide" />
       </button>
     </div>
   );
 };
-
 export default Carousel;
