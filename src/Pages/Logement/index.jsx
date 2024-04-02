5af43b6f8c212f90a4baf53932d7b31917e88e38
Zain <zain.frayha@gmail.com>
2024-04-03 00:45:24 +0200

    done with styling logement.

--- src/Pages/Logement/index.jsx
+++ src/Pages/Logement/index.jsx
@@ -25,30 +25,42 @@ function Logement() {
                 <div className="logement__main" key={logement.id}>
                   <Carousel images={logement["pictures"]} />
 
-                  <h2 className="logement__title">{logement.title}</h2>
-                  <h3 className="logement__location">{logement.location}</h3>
-                  <div className="logement__tags--div">
-                    {logement.tags.map((tag, index) => (
-                      <div key={index}>
-                        <span className="logement__tags--span">{tag}</span>
+                  <div className="logement__main__wraper">
+                    <div className="logement__main__wraper__first">
+                      <h2 className="logement__title">{logement.title}</h2>
+                      <h3 className="logement__location">
+                        {logement.location}
+                      </h3>
+                      <div className="logement__tags--div">
+                        {logement.tags.map((tag, index) => (
+                          <div key={index}>
+                            <span className="logement__tags--span">{tag}</span>
+                          </div>
+                        ))}
                       </div>
-                    ))}
-                  </div>
-                  <div className="logement__rating-owner">
-                    <Rating
-                      rating={parseInt(logement.rating)}
-                      className="logement__rating-div"
-                    />
+                    </div>
 
-                    <div className="logement__owner">
-                      {logement.host["name"]}
-                      <img
-                        className="logement__owner-pic"
-                        src={logement.host["picture"]}
-                        alt="the owner"
-                      />
+                    <div className="logement__main__wraper__second">
+                      <div className="logement__rating-owner">
+                        <Rating
+                          rating={parseInt(logement.rating)}
+                          className="logement__rating-div"
+                        />
+
+                        <div className="logement__owner">
+                          {logement.host["name"].split(" ")[0]}
+                          <br />
+                          {logement.host["name"].split(" ")[1]}
+                          <img
+                            className="logement__owner-pic"
+                            src={logement.host["picture"]}
+                            alt="the owner"
+                          />
+                        </div>
+                      </div>
                     </div>
                   </div>
+
                   <div className="logement__description">
                     <DescriptionCollapse description={logement.description} />
                     <EquipmentCollapse equipment={logement.equipments} />
