93a55351cbb3caee7dba758060bcf45dcf742b22
Zain <zain.frayha@gmail.com>
2024-04-17 17:26:45 +0200

    Render 404 page  without changing URL.

--- src/Pages/Logement/index.jsx
+++ src/Pages/Logement/index.jsx
@@ -1,47 +1,50 @@
-import React from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 import { useFetch } from "../../utils/hooks";
 import "../../style/css/main.css";
-// import Rating from "../../components/Rating";
-// import Carousel from "../../components/Carousel";
-// import UniversalCollapsible from "../../components/Collapse/index";
 import Card from "../../components/Card";
-
+import Erorr from "../../components/Error";
 function Logement() {
   const { logementId } = useParams();
-  const navigate = useNavigate();
   const { isLoading, data, error } = useFetch(`/logements.json`);
+  const [notFound, setNotFound] = useState(false);
 
   React.useEffect(() => {
-    if (
-      !isLoading &&
-      data &&
-      !data.some((logement) => logement.id === logementId)
-    ) {
-      navigate("/404");
+    if (!isLoading && data) {
+      const logementExists = data.some(
+        (logement) => logement.id === logementId
+      );
+      setNotFound(!logementExists);
     }
-  }, [isLoading, data, logementId, navigate]);
+  }, [isLoading, data, logementId]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error occurred while fetching data</p>;
+  }
+
+  if (notFound) {
+    return <Erorr />;
+  }
 
   return (
     <div>
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Error occurred while fetching data</p>}
-      {!isLoading && !error && (
-        <div>
-          {data.map((logement) => {
-            if (logement.id === logementId) {
-              return (
-                <div className="logement__main" key={logement.id}>
-                  <Card logement={logement} />
-                  <div className="logement__description"></div>
-                </div>
-              );
-            }
-            return null;
-          })}
-        </div>
-      )}
+      {data.map((logement) => {
+        if (logement.id === logementId) {
+          return (
+            <div className="logement__main" key={logement.id}>
+              <Card logement={logement} />
+              <div className="logement__description"></div>
+            </div>
+          );
+        }
+        return null;
+      })}
     </div>
   );
 }
+
 export default Logement;
